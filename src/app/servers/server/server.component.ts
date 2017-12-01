5b9dbdec1a40d138770a873e434981bc33a05edf
mpedersen15 <mpedersen@mindrocketdesign.com>
2017-12-01 15:56:36 -0700

    initial commit

---
+++ src/app/servers/server/server.component.ts
@@ -0,0 +1,30 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+import { ServersService } from '../servers.service';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+  server: {id: number, name: string, status: string};
+
+  constructor(private serversService: ServersService, private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit() {
+    const serverId = +this.route.snapshot.params.id;
+    this.server = this.serversService.getServer(serverId);
+
+    this.route.params.subscribe(params => {
+      const id = +params.id;
+      this.server = this.serversService.getServer(id);
+    });
+  }
+
+  onEdit() {
+    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve' });
+  }
+
+}
